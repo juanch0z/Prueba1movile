3281683bb43029b0044e9c11709c15799d90cd8c
juanch0z <jua.medinap@duocuc.cl>
2023-09-10 22:45:33 -0300

    Se agrega el Proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'registro',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
